6fdf2055974dd7a10a3c1067fadc029bc4d43eec
Charles Ssekitto <charles.ssekitto@cobblestoneenergy.com>
2021-03-28 20:23:56 +0100

    add service workers to the website

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = "bouge-pwa";
+
+const filesToCache = [
+  "/",
+  "/views/index.html",
+  "/js/index.js",
+  "/css/styles.css",
+];
+
+/**
+ * Start the service work and cache all of the app's content
+ */
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+/**
+ * Serve cache content when offline
+ */
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
